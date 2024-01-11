63b0bbbaaf0c2d0a8968d59dc149b752c6c4908f
Cerchie <lcerchie@confluent.io>
2024-01-11 14:45:20 -0700

    initial

---
+++ setupsocket.py
@@ -0,0 +1,46 @@
+#https://pypi.org/project/websocket_client/
+from confluent_kafka import Producer
+from configparser import ConfigParser
+from dotenv import load_dotenv
+
+import os
+import rel
+import socket
+import websocket
+
+load_dotenv()
+
+FINNHUB_KEY = os.environ['FINNHUB_KEY']
+
+config_parser = ConfigParser(interpolation=None)
+config_file = open('config.properties', 'r')
+config_parser.read_file(config_file)
+client_config = dict(config_parser['kafka_client'])
+
+
+producer = Producer(client_config)
+
+def on_message(ws, stockname):
+    producer.produce(stockname)
+
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws):
+    print("### closed ###")
+
+def on_open(ws,stockname):
+    ws.send(f'{"type":"subscribe","symbol":{stockname}}')
+
+def on_select(stockname):
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp(f'wss://ws.finnhub.io?token={FINNHUB_KEY}',
+                              on_message = on_message(ws, stockname=stockname),
+                            #   SET UP WRONG, NEED TO FIND WAY TO PASS STOCKNAME
+                              on_error = on_error,
+                              on_close = on_close)
+    ws.on_open = on_open(ws, stockname=stockname)
+    ws.run_forever()
+
+    rel.signal(2, rel.abort)  # Keyboard Interrupt
+    rel.dispatch()
