b13631b47dc4d463e581798516922ac7db22e1fc
Cerchie <lcerchie@confluent.io>
2024-01-12 14:19:23 -0700

    UI now triggers ws

--- setupsocket.py
+++ setupsocket.py
@@ -20,26 +20,29 @@ client_config = dict(config_parser['kafka_client'])
 
 producer = Producer(client_config)
 
-def on_message(ws, stockname):
-    producer.produce(stockname)
-
-def on_error(ws, error):
-    print(error)
-
-def on_close(ws):
-    print("### closed ###")
-
-def on_open(ws,stockname):
-    ws.send(f'{"type":"subscribe","symbol":{stockname}}')
-
 def on_select(stockname):
+
+    def on_message(ws, message):
+        producer.produce(stockname, message)
+
+    def on_error(ws, error):
+        print(error)
+
+    def on_close(ws):
+        print("### closed ###")
+
+    def on_open(ws):
+        ws.send(f'{{"type":"subscribe","symbol":"{stockname}"}}')
+  
+
+
     websocket.enableTrace(True)
+
     ws = websocket.WebSocketApp(f'wss://ws.finnhub.io?token={FINNHUB_KEY}',
-                              on_message = on_message(ws, stockname=stockname),
-                            #   SET UP WRONG, NEED TO FIND WAY TO PASS STOCKNAME
+                              on_message = on_message,
                               on_error = on_error,
                               on_close = on_close)
-    ws.on_open = on_open(ws, stockname=stockname)
+    ws.on_open = on_open
     ws.run_forever()
 
     rel.signal(2, rel.abort)  # Keyboard Interrupt
